e0ef353ff0e2c91d0b5ab1e7e884f5e02584d090
Luis' New Computer <lrodriguezmoldes@gmail.com>
2020-04-07 20:04:52 +0200

    Token Auth Expire 2

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -131,18 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-#     '/static/',
-# )
-
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "media"),
-    '/media/',
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
 )
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
